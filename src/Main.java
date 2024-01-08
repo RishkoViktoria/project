d0f79c94f0f052b3573db7d790c26e857a493a6e
viktoria <rvmpalvelu@yahoo.com>
2024-01-08 20:30:33 +0200

    **"Added solution for bonus calculation on account top-up"**

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int currenBalance = 100; // Начальный счет клиента
+        int currentBalance = 100; // Начальный счет клиента
 
         int topUpAmount = 1100; // Сумма пополнения
 
-        if (topUpAmount > 1000) {
-            int bonus = (topUpAmount - 1000) / 100; // Расчет бонуса currentBalance += topUpAmount + bonus;
+        if (topUpAmount >= 1000) {
+            int bonus = (topUpAmount - 1000) / 100; // Расчет бонуса
+               currentBalance += topUpAmount + bonus;
 
             System.out.println("Бонус: " + bonus + " рублей");
         } else {
-            currenBalance += topUpAmount; // Итоговый счет без бонуса
+            currentBalance += topUpAmount; // Итоговый счет без бонуса
         }
-        System.out.println("Итоговый счет: " + currenBalance + " рублей");
+        System.out.println("Итоговый счет: " + currentBalance + " рублей");
         }
 
 }
